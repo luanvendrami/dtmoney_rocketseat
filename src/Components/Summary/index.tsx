38c9e06552a4620e33d3831f500db921d630f7c8
luanvendrami <luan.vendrami@hotmail.com>
2022-02-20 21:30:04 -0300

    Adicionado variável para usar o context de transação

---
+++ src/Components/Summary/index.tsx
@@ -0,0 +1,36 @@
+import { useContext } from 'react';
+import { TransactionsContext } from '../../TransactionsContext';
+import entradaImg from '../../assents/Entradas.svg';
+import saidaImg from '../../assents/Saídas.svg';
+import totalImg from '../../assents/Total.svg';
+import { Container } from "./style";
+
+export function Summary(){
+  const { transactions } = useContext(TransactionsContext);
+
+    return(
+      <Container>
+        <div>
+          <header>
+            <p>Entradas</p>
+            <img src={entradaImg} alt="Entradas" />
+          </header>
+          <strong>R$1000,00</strong>
+        </div>
+        <div>
+          <header>
+            <p>Saidas</p>
+            <img src={saidaImg} alt="Saidas" />
+          </header>
+          <strong>-R$500,00</strong>
+        </div>
+        <div className='highlight-background'>
+          <header>
+            <p>Total</p>
+            <img src={totalImg} alt="Total" />
+          </header>
+          <strong>R$500,00</strong>
+        </div>
+      </Container>
+    )
+}
